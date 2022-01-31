09b471a22f6b6f1d7fb4062dea0acbacdd22191d
Maseu <maseu@MacBook-Pro-Maseu.local>
2022-01-31 14:46:59 +0100

    cleanup

---
+++ src/components/Keypad/Keypad.styles.tsx
@@ -0,0 +1,66 @@
+import { css } from '@emotion/css'
+import { Colors } from '../../consts/colors'
+
+export const keypadContainer = css({
+    background: Colors.DARKGREY,
+    width: '100%',
+    height: '70vh',
+    display: 'grid',
+    gap: 2,
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gridTemplaterows: 'repeat(5, 1fr)',
+    gridTemplateAreas: `'features features features operations' 
+                        'numbers numbers numbers operations'
+                        'numbers numbers numbers operations'
+                        'numbers numbers numbers operations'
+                        'numbers numbers numbers operations'
+                        `,
+    button: {
+        background: 'none',
+        border: 'none',
+        fontSize: '1.2rem',
+        fontWeight: 'medium',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+        transition: 'background .2s ease',
+        ':hover': {
+            background: 'lightgrey',
+        },
+    },
+})
+
+export const functionBtns = css({
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    gridArea: 'features',
+    gap: 2,
+    button: {
+        background: Colors.PINK,
+    },
+})
+
+export const numberBtns = css({
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    gridArea: 'numbers',
+    gap: 2,
+    button: {
+        background: Colors.GREY,
+    },
+})
+
+export const zeroBtn = css({
+    gridColumn: '1 / 3',
+})
+
+export const operationBtns = css({
+    display: 'grid',
+    gridTemplateColumns: '1fr',
+    gridArea: 'operations',
+    gap: 2,
+    button: {
+        background: Colors.NEON_PURPLE,
+    },
+})
--- src/components/Keypad/Keypad.tsx
+++ src/components/Keypad/Keypad.tsx
@@ -1,65 +1,20 @@
-import { css } from '@emotion/css'
 import React from 'react'
-import { Colors } from '../../consts/colors'
+import {
+    functionBtns,
+    keypadContainer,
+    numberBtns,
+    operationBtns,
+    zeroBtn,
+} from './Keypad.styles'
 
 export const Keypad = () => (
-    <div
-        className={css({
-            background: Colors.DARKGREY,
-            width: '100%',
-            height: '70vh',
-            display: 'grid',
-            gap: 2,
-            gridTemplateColumns: 'repeat(4, 1fr)',
-            gridTemplaterows: 'repeat(5, 1fr)',
-            gridTemplateAreas: `'features features features operations' 
-                                'numbers numbers numbers operations'
-                                'numbers numbers numbers operations'
-                                'numbers numbers numbers operations'
-                                'numbers numbers numbers operations'
-                                `,
-            button: {
-                background: 'none',
-                border: 'none',
-                fontSize: '1.2rem',
-                fontWeight: 'medium',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                color: 'white',
-                transition: 'background .2s ease',
-                ':hover': {
-                    background: 'lightgrey',
-                },
-            },
-        })}
-    >
-        <div
-            className={css({
-                display: 'grid',
-                gridTemplateColumns: '1fr 1fr 1fr',
-                gridArea: 'features',
-                gap: 2,
-                button: {
-                    background: Colors.PINK,
-                },
-            })}
-        >
+    <div className={keypadContainer}>
+        <div className={functionBtns}>
             <button>C</button>
             <button>LCM</button>
             <button>GCD</button>
         </div>
-        <div
-            className={css({
-                display: 'grid',
-                gridTemplateColumns: '1fr 1fr 1fr',
-                gridArea: 'numbers',
-                gap: 2,
-                button: {
-                    background: Colors.GREY,
-                },
-            })}
-        >
+        <div className={numberBtns}>
             <button>9</button>
             <button>8</button>
             <button>7</button>
@@ -69,26 +24,10 @@ export const Keypad = () => (
             <button>3</button>
             <button>2</button>
             <button>1</button>
-            <button
-                className={css({
-                    gridColumn: '1 / 3',
-                })}
-            >
-                0
-            </button>
+            <button className={zeroBtn}>0</button>
             <button>,</button>
         </div>
-        <div
-            className={css({
-                display: 'grid',
-                gridTemplateColumns: '1fr',
-                gridArea: 'operations',
-                gap: 2,
-                button: {
-                    background: Colors.NEON_PURPLE,
-                },
-            })}
-        >
+        <div className={operationBtns}>
             <button>/</button>
             <button>*</button>
             <button>+</button>
